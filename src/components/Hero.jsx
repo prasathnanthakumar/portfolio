d996962cdc0f14b0cb87e11210198285eb111e81
prasath <152866272+prasathnanthakumar@users.noreply.github.com>
2025-02-04 22:42:35 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
         </button>
         <button
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white block transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
-          <a href="https://drive.google.com/file/d/1Q-Gzs2XFgLBoxNw_J9j0IxrNTCbqf_5f/view?usp=drive_link">Resume</a>
+          <a href="https://drive.google.com/file/d/1OZrpw47NnF5ao24WNdiNOcoZpG91safo/view?usp=sharing">Resume</a>
         </button>
       </div>
 
